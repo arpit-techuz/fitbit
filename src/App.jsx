06ff97423a1aa3442cfdd274ec91305dad3655ca
Arpit Dobariya <arpit@techuz.com>
2024-07-15 12:17:46 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Navigation from '../components/Navigation'
+
+function App() {
+  return (
+    <>
+      <Navigation />
+    </>
+  )
+}
+
+export default App
