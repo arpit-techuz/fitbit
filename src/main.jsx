6b94b4b986dbe456b6b1b845b396e61eb2a1a481
Your Name <you@example.com>
2024-07-15 13:24:17 +0530

    samyak coomit

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 import './custom.scss'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>,
 )
